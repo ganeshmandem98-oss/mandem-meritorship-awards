6e6b81f81cf188427f1299e3ecdc8f42eb2ca616
ganeshmandem98-oss <ganesh.mandem98@gmail.com>
2026-07-17 13:54:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function updateDateTime(){
 
+let now=new Date();
+
+document.getElementById("date").innerHTML=
+"📅 "+now.toLocaleDateString();
+
+document.getElementById("time").innerHTML=
+"🕒 "+now.toLocaleTimeString();
+
+}
+
+setInterval(updateDateTime,1000);
+
+updateDateTime();
